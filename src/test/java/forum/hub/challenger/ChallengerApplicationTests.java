b58a6e755faa18aa4beae98697b696dd1b268c8f
João Victor <jv-oliveira07@hotmail.com>
2025-08-19 17:21:13 -0300

    first commit

---
+++ src/test/java/forum/hub/challenger/ChallengerApplicationTests.java
@@ -0,0 +1,13 @@
+package forum.hub.challenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
